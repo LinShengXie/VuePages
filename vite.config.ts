7c7258c520397bf06303f222c2ab56c459570816
xieyijia <xlin84395@gmail.com>
2024-07-09 17:29:11 +0800

    feat:打包测试

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-    // 静态资源基础路径 base: './' || '',
-    base: '/VuePages/',
+    // 静态资源基础路径 base: './' : '',
+    base: process.env.NODE_ENV === 'production' ?'./' : '',
     resolve:{
       alias: {
         // 配置目录别名
